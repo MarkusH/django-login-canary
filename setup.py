9db50f48cb13eac70a29e9c88880991b19e7467e
Markus Holtermann <info@markusholtermann.eu>
2015-04-16 02:34:11 +0200

    Bump to beta

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'Django>=1.8',
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
