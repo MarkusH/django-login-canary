7267a1701ea1701ade79c23de3d300d60ad7956d
Markus Holtermann <info@markusholtermann.eu>
2015-04-16 02:54:57 +0200

    Added .coveragerc

--- login_canary/settings.py
+++ login_canary/settings.py
@@ -6,5 +6,5 @@ NOTIFY_LOGGED_IN = getattr(settings, 'LOGIN_CANARY_NOTIFY_LOGGED_IN', True)
 NOTIFY_LOGIN_FAILED = getattr(settings, 'LOGIN_CANARY_NOTIFY_LOGIN_FAILED', False)
 
 SERVICE_NAME = getattr(settings, 'LOGIN_CANARY_SERVICE_NAME')
-if SERVICE_NAME is None:
+if SERVICE_NAME is None:  # pragma: no cover
     raise ImproperlyConfigured('You need to set LOGIN_CANARY_SERVICE_NAME.')
